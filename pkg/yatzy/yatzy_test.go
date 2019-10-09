f5f209cba4de927c46e0d34a724269f20951079e
Kim Tore Jensen <kimtjen@gmail.com>
2019-10-09 17:38:19 +0200

    add some tests and benchmarks

--- pkg/yatzy/yatzy_test.go
+++ pkg/yatzy/yatzy_test.go
@@ -1,8 +1,11 @@
 package yatzy_test
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
+	"time"
 	"yatzy/pkg/yatzy"
 )
 
@@ -19,4 +22,60 @@ func TestDice_Score(t *testing.T) {
 
 	assert.Equal(t, yatzy.Score(18), dice.Score(6))
 	assert.Equal(t, yatzy.Score(4), dice.Score(4))
+}
+
+func random() yatzy.Die {
+	return yatzy.Die(rand.Intn(6) + 1)
+}
+
+func roll(n int) yatzy.Dice {
+	dice := make(yatzy.Dice, n)
+	for i := range dice {
+		dice[i] = random()
+	}
+	return dice
+}
+
+func BenchmarkGame(b *testing.B) {
+
+	var dice yatzy.Dice
+	var keep yatzy.Dice
+	var err error
+
+	rand.Seed(time.Now().UnixNano())
+	game := yatzy.NewGame()
+
+	for i := 0; i < b.N; i++ {
+		for err == nil {
+			dice = roll(5 - len(keep))
+			dice = append(keep, dice...)
+			keep, err = game.Roll(dice)
+		}
+		game.Results()
+	}
+}
+
+func TestAverage(t *testing.T) {
+
+	var total int
+
+	const max = 10000000
+	var dice yatzy.Dice
+	var keep yatzy.Dice
+	var err error
+
+	rand.Seed(time.Now().UnixNano())
+	game := yatzy.NewGame()
+
+	for i := 0; i < max; i++ {
+		for err == nil {
+			dice = roll(5 - len(keep))
+			dice = append(keep, dice...)
+			keep, err = game.Roll(dice)
+		}
+		results := game.Results()
+		total += int(results.Total)
+	}
+
+	fmt.Printf("Average score is %d\n", total/max)
 }
